8087fe7fe398f90e38c92b87d5e5867a7cb0b9bc
ishanpandey <ishan.pandey@infra.market>
2023-10-24 18:15:42 +0530

    Add chain of responsibility

--- design_patterns/go/behavioral/behavorial.md
+++ design_patterns/go/behavioral/behavorial.md
@@ -24,7 +24,25 @@ https://refactoring.guru/design-patterns/behavioral-patterns
           * And then you iterate through this map, and perform notifications in a regularised way.
         
 2. **Chain Of Responsibility**
-    * 
+    * Mirrored a lot in real-world
+    * examples
+      * Unethical behaviour by an employee, who takes blame?
+        1. Employee
+        2. Manager
+        3. CEO
+      * You click on graphical element on a form, who gets to handle the event?
+        1. Button handles it, it stops further processing
+        2. Group box - button does nothing, group box underlying handles event
+        3. Underlying window - button, underlying group box do nothing, window handles it
+      * CCG computer game
+        1. Creature has attack and defense values.
+        2. Can get booster cards - how to apply these values to creature?
+    * Chain of Responsibility: chain of components, who all get a chance to process a command or a query; optionally having default processing implementation and ability to terminate processing chain (every element of chain can have).
+    * Linked List of method invocations
+    * Command Query Separation:
+      * Command: Mutable - action or query which changes value
+      * Query: Immutable - asking for information
+
 3. **Strategy**
    * Many algorithms can be broken down into high level and low level parts
    * High level algorithms can be reused for different purposes. Low level parts are specific to different use-case.
--- design_patterns/go/behavioral/chain-of-responsibility/method_chain.go
+++ design_patterns/go/behavioral/chain-of-responsibility/method_chain.go
@@ -16,14 +16,16 @@ func (c *Creature) String() string {
 }
 
 /*
+	Abstract Modifier
 	When you add a modifier, you add it on top of an existing modifier
+	And we want to build a stack/list of these modifications, so that these can be applied to creature one after other
 */
 type Modifier interface {
-	Add(m Modifier)
-	Apply()
+	Add(m Modifier) // adds additional modifier to chain of responsibility, attaching it to end of already applied modifiers
+	Apply()         // modifier gets applied when this method is called
 }
 
-// is effectively singly linked list, and implements Modifier interface
+// CreatureModifier concrete type - what creature this Modifier is applied to
 type CreatureModifier struct {
 	creature *Creature
 	next     Modifier
@@ -31,16 +33,98 @@ type CreatureModifier struct {
 
 func (c *CreatureModifier) Add(m Modifier) {
 	if c.next != nil {
-		c.next.Add(m)
+		c.next.Add(m) // add element to the end
 	} else {
-		c.next = m
+		c.next = m // replace a modifier
 	}
 }
 
+// Apply is just calling other modifiers on this, and is actually not doing anything by itself.
+// Reason is: we are going to aggregate it and make use of it
 func (c *CreatureModifier) Apply() {
-
+	if c.next != nil {
+		c.next.Apply()
+	}
 }
 
-func main() {
+func NewCreatureModifier(creature *Creature) *CreatureModifier { // Factory for CreatureModifier
+	return &CreatureModifier{creature: creature}
+}
 
+/*
+	Concrete modifier definitions
+	We are initialising the aggregated part for creature modifier
+	We first initialise
+*/
+type DoubleAttackModifier struct {
+	CreatureModifier
+}
+
+func NewDoubleAttackModifier(c *Creature) *DoubleAttackModifier {
+	return &DoubleAttackModifier{
+		CreatureModifier{creature: c},
+	}
+}
+
+func (d *DoubleAttackModifier) Apply() {
+	fmt.Println("doubling ", d.creature.Name, " attack value")
+	d.creature.Attack *= 2
+	d.CreatureModifier.Apply() // => this will call (c *CreatureModifier) Apply()
+}
+
+/*
+	Increase Defense Modifier
+*/
+type IncreaseDefenseModifier struct {
+	CreatureModifier
+}
+
+func NewIncreasedDefenseModifier(c *Creature) *IncreaseDefenseModifier {
+	return &IncreaseDefenseModifier{CreatureModifier{creature: c}}
+}
+
+func (i *IncreaseDefenseModifier) Apply() {
+	if i.creature.Attack <= 200 {
+		fmt.Println("Increasing ", i.creature.Attack, " defense")
+		i.creature.Defense++
+	}
+
+	i.CreatureModifier.Apply()
+}
+
+/*
+	No bonuses modifier - Apply() forgets to call next.Apply() that means every single modifier after this is not going to be applied
+*/
+type NoBonusesModifier struct {
+	CreatureModifier
+}
+
+func NewNoBonusesModifier(c *Creature) *NoBonusesModifier {
+	return &NoBonusesModifier{CreatureModifier{creature: c}}
+}
+
+func (n *NoBonusesModifier) Apply() {}
+
+func main() {
+	goblin := NewCreature("Goblin", 1, 1)
+	rootNode := NewCreatureModifier(goblin)       // first modifier
+	rootNode.Add(NewDoubleAttackModifier(goblin)) // second modifier
+	rootNode.Apply()                              // applies all the modifiers from LL
+	fmt.Println(goblin.String())
+	rootNode.Add(NewDoubleAttackModifier(goblin))
+	rootNode.Add(NewDoubleAttackModifier(goblin))
+	rootNode.Apply()
+	fmt.Println(goblin.String())
+
+	rootNode.Add(NewIncreasedDefenseModifier(goblin)) // runs all the modifiers on rootNode again, on previous value - root first modifier -----> latest modifier
+	rootNode.Apply()
+	fmt.Println(goblin.String())
+
+	goblin2 := NewCreature("Goblin", 1, 1)
+	rootNode2 := NewCreatureModifier(goblin2)           // first modifier
+	rootNode2.Add(NewDoubleAttackModifier(goblin2))     // second modifier
+	rootNode2.Add(NewNoBonusesModifier(goblin2))        // no bonus modifier - aage ke sare modifiers will be disabled
+	rootNode2.Add(NewIncreasedDefenseModifier(goblin2)) // fourth modifier
+	rootNode2.Apply()
+	fmt.Println(goblin2.String())
 }
